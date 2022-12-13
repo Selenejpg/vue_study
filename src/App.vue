e07e591e9661d01084f59b096c391ad01a2c232c
noemipintaldi <noemipintaldi@gmail.com>
2022-12-13 14:27:46 +0100

    initial push

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TestComp/>  
+  </div>
+</template>
+
+<script>
+import "bootstrap"
+import TestComp from './components/TestComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TestComp
+  }
+}
+</script>
+
+<style lang="scss">
+@import "bootstrap/dist/css/bootstrap.min.css";
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
